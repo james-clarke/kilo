9424bd1dfe0bef729a107b8ea89b704ac2a21df2
James Clarke <james.clarke@tcu.edu>
2024-09-04 16:20:25 -0500

    disable sig-int, etc. print input to screen

--- kilo.c
+++ kilo.c
@@ -1,31 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 
 struct termios orig_termios;
 
-void disableRawMode()
+void disableRawMode(void)
 {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-
-void enableRawMode()
+void enableRawMode(void)
 {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main()
+int main(void)
 {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+
   return 0;
 }
