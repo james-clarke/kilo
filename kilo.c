ebba791765fef3315600fad85a57d63e317a3a4a
James Clarke <james.clarke@tcu.edu>
2024-09-05 13:03:33 -0500

    refactor

--- kilo.c
+++ kilo.c
@@ -6,36 +6,30 @@
 
 struct termios orig_termios;
 
-void disableRawMode(void)
-{
+void disableRawMode(void){
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enableRawMode(void)
-{
+void enableRawMode(void){
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(IXON);
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_lflag &= ~(IXON | ICRNL);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main(void)
-{
+int main(void){
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-  {
-    if (iscntrl(c))
-    {
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if (iscntrl(c)){
       printf("%d\n", c);
     }
-    else
-    {
+    else{
       printf("%d ('%c')\n", c, c);
     }
   }
