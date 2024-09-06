68678d0e8d53745346bab9112733e0d6c32e9b9e
James Clarke <james.clarke@tcu.edu>
2024-09-06 13:41:58 -0500

    fix reaad timeout

--- kilo.c
+++ kilo.c
@@ -15,13 +15,12 @@ void enableRawMode(void){
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(IXON | ICRNL | INPCK | ISTRIP | BRKINT);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_lflag |= (CS8);
-  raw.c_lflag &= ~(OPOST);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
-
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
